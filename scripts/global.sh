48f352addbcad7a6a3d4b92eb5bb6ef4d7fda804
jiangshixiong <shixiong@buffalo.edu>
2016-03-07 12:33:38 -0500

    add gem5-cpu

---
+++ scripts/global.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# File Name: global.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Mon 29 Feb 2016 11:18:05 PM EST
+#########################################################################
+#!/bin/bash
+
+export gem5_tutorial="../gem5-stable/configs/tutorial/"
+export gem5_cache_tags="../gem5-stable/src/mem/cache/tags/"
+export gem5_root="../gem5-table/"
+export gem5_scripts=$PWD
--- scripts/play.sh
+++ scripts/play.sh
@@ -20,12 +20,60 @@ binary="${binary_path}/gem5.opt";
 python_path="${gem5}/configs/example";
 python="${python_path}/se.py";
 
+# -------------------------CPU---------------------------
+cpu_type="--cpu-type DerivO3CPU";
+num_cpu="--num-cpu 1";
+sys_voltage="--sys-voltage 1.0V"
+sys_clock="--sys-clock 1GHz";
+cpu_clock="--cpu-clock 2GHz";
+
+CPU_SETTINGS="${cpu_type} ${num_cpu} ${sys_voltage} ${sys_clock} ${cpu_clock}";
+
+# --------------------------CACHES---------------------------
+caches="--caches";
+l2cache="--l2cache";
+
+# ----------l1----------
+l1d_size="--l1d-size 64KB";
+l1d_assoc="--l1d-assoc 2";
+
+l1i_size="--l1i-size 32KB";
+l1i_assoc="--l1i-assoc 2";
+
+L1_SETTINGS="${l1d_size} ${l1d_assoc} ${l1i_size} ${l1i_assoc}";
+
+# ----------l2----------
+num_l2caches="--num_l2caches 1";
+l2_size="--l2-size 2MB";
+
+L2_SETTINGS="${num_l2caches} ${l2_size}";
+
+# ----------l3----------
+num_l3caches="--num_l3caches 1";
+l3_size="--l3-size 3MB";
+
+L3_SETTINGS="${num_l3caches} ${l3_size}";
+
+# ---------- l1 + l2 + l3 -------------
+CACHES_SETTINGS="${L1_SETTINGS} ${L2_SETTINGS} ${L3_SETTINGS}";
+
+# -----------------------MEMORY-------------------------
+mem_type="--mem-type DDR3_1600_x64";
+mem_channels="--mem-channels 1";
+mem_ranks="--mem-ranks ";
+mem_size="--mem-size 512MB";
+
+MEMORY_SETTINGS="${mem_type} ${mem_channels} ${mem_ranks} ${mem_size}";
+
+# ------------------CPU + CACHES + MEMORY -------------------
+SETTINGS="${CPU_SETTINGS} ${CACHES_SETTINGS} ${MEMORY_SETTINGS}";
+
+# ---------------------- BENCHMARKS ------------------------
 benchmark_path="${gem5}/tests/test-progs/hello/bin/${tech_lowercase}/linux";
 benchmark="${benchmark_path}/hello";
 
-commandline="${binary} --debug-flags=Cache --debug-file=debug_out.txt ${python} -c ${benchmark} --cache";
-
+# ---------------------- COMMANDLINE --------------------------
+commandline="${binary} ${python} ${SETTINGS} -c ${benchmark}";
 echo ${commandline};
-
 ${commandline};
 
